84244360e0881e64e3981b2d190c7c12cb0b207f
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-26 23:10:51 -0300

    setting router

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './Content.css'
+import { Routes, Route } from "react-router-dom";
+
+//routes é um SWITCH
+
+import Home from "../../views/examples/Home";
+import About from "../../views/examples/About";
 
 const Content = props => (
 
         <main className="Content">
-            <h1>Content</h1>
+            <Routes>
+                <Route path="/about" element={<About />} />
+                <Route path="/" exact element={<Home />} />
+            </Routes>
         </main>
 )
 
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import './App.css'
+
+import { BrowserRouter } from  'react-router-dom'
+
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
 
 const App = props => (
 
         <div className="App">
-            <Menu></Menu>
-            <Content></Content>
+            <BrowserRouter>
+                <Menu></Menu>
+                <Content></Content>
+            </BrowserRouter>
+
         </div>
 )
 
